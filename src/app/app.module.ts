3de2e1c6d3a5a12b883c3194a89f249b16c5261a
emddeveloper <emd.developer@gmail.com>
2021-03-28 10:33:49 +0530

    lazy loading implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CollectionsModule } from './collections/collections.module';
-import { ElementsModule } from './elements/elements.module';
-import { ViewsModule } from './views/views.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [
-    BrowserModule,
-    CollectionsModule,
-    ElementsModule,
-    ViewsModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
