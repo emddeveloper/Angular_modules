ce4feadfc0338b9a9c5364a6e4684d629eede7db
emddeveloper <emd.developer@gmail.com>
2021-03-30 11:38:29 +0530

    children routing added

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BioComponent } from './bio/bio.component';
 import { CollectionHomeComponent } from './collection-home/collection-home.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CollectionHomeComponent,
+    children: [
+      {
+        path: '',
+        component: BioComponent,
+      },
+      {
+        path: 'companies',
+        component: CompaniesComponent,
+      },
+      {
+        path: 'partners',
+        component: PartnersComponent,
+      },
+    ],
   },
 ];
 
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,9 +5,12 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionHomeComponent } from './collection-home/collection-home.component';
 import { SharedModule } from '../shared/shared.module';
 import { TableComponent } from './table/table.component';
+import { BioComponent } from './bio/bio.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 @NgModule({
-  declarations: [CollectionHomeComponent, TableComponent],
+  declarations: [CollectionHomeComponent, TableComponent, BioComponent, CompaniesComponent, PartnersComponent],
   imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   exports: [],
 })
